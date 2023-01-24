f561345787a983729dacf0d352db53e908d2a498
iankaFil <108305015+iankaFil@users.noreply.github.com>
2023-01-24 13:26:48 -0700

    get new

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,12 +7,7 @@ import Filter from 'components/Filter';
 
 class App extends Component {
   state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -72,7 +67,9 @@ class App extends Component {
     const { filter } = this.state;
     return (
       <div className={css.container}>
-        <h1 className={css.title}>Phonebook</h1>
+        <h1 className={css.title}>
+          Phone<span className={css.titleAccent}>book</span>
+        </h1>
         <ContactForm onSubmit={this.addName} />
 
         <h2 className={css.subtitle}>Contacts</h2>
